f3074da330c19b360a34fc1321182125c5c5d480
Oliver Blaser <kontakt@oblaser.ch>
2022-01-27 17:31:46 +0100

    cmake system tests in separate dir

--- build/run_test.sh
+++ build/run_test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # author        Oliver Blaser
-# date          26.01.2022
+# date          27.01.2022
 # copyright     MIT - Copyright (c) 2022 Oliver Blaser
 
-# Options:
-# -s    also run system test executables
 
 
 errCnt=0
@@ -18,17 +16,6 @@ function onError()
 
 
 cd cmake
-
-if [ "$1" == "-s" ]
-then
-    echo -e "\033[96momw-system-test-cli-shared\033[39m"
-    ./omw-system-test-cli-shared
-    if [ $? -ne 0 ]; then onError; fi;
-    
-    echo -e "\033[96momw-system-test-cli-static\033[39m"
-    ./omw-system-test-cli-static
-    if [ $? -ne 0 ]; then onError; fi;
-fi
 
 echo -e "\033[96momw-unit-test-shared\033[39m"
 ./omw-unit-test-shared
