f1ff2baabd1c36f4af4197ce060ecdb9efc8da5a
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2024-05-21 13:07:48 +0200

    made usable (build and link) by parent CMakeLists.txt

---
+++ build/cmake/libomw.var.cmake
@@ -0,0 +1,29 @@
+# author        Oliver Blaser
+# date          21.05.2024
+# copyright     MIT - Copyright (c) 2024 Oliver Blaser
+
+set(BINNAME omw)
+
+set(SOURCES
+${CMAKE_CURRENT_LIST_DIR}/../../src/io/configFile.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/io/file.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/io/serialPort.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/envVar.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/error.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/exception.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/resources.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/string.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/windows/windows.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/algorithm.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/checksum.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/cli.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/color.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/encoding.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/int.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/math.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/omw.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/string.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/utility.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/vector.cpp
+${CMAKE_CURRENT_LIST_DIR}/../../src/version.cpp
+)
